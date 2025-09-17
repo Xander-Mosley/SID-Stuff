28843e3b422b118cbac18e11a3543bb06a288932
Xander D Mosley <XanderDMosley.Engineer+GitHub@gmail.com>
2025-09-17 11:54:21 -0500

    Post-trip push

--- flight_performance_plotter.py
+++ flight_performance_plotter.py
@@ -188,9 +188,61 @@ def plot_rates(
     pit_rate = dataframe["gy"]
     yaw_rate = dataframe["gz"]
     
-    axs[0].plot(time, rol_rate, label='Roll', color="blue")
-    axs[1].plot(time, pit_rate, label='Pitch', color="red")
-    axs[2].plot(time, yaw_rate, label='Yaw', color="green")
+    axs[0].scatter(time, rol_rate, label='Roll', color="blue")
+    axs[1].scatter(time, pit_rate, label='Pitch', color="red")
+    axs[2].scatter(time, yaw_rate, label='Yaw', color="green")
+    
+    # --- Final Formatting ---
+    axs[0].set_title("Roll Rate Over Time")
+    axs[0].set_ylabel("Rate\n[rad/s]")
+    axs[1].set_title("Pitch Rate Over Time")
+    axs[1].set_ylabel("Rate\n[rad/s]")
+    axs[2].set_title("Yaw Rate Over Time")
+    axs[2].set_ylabel("Rate\n[rad/s]")
+    
+    for ax in axs:
+        ax.grid(True)
+        ax.legend(loc='upper right', fontsize='medium')
+    axs[-1].set_xlabel("Time [s]")
+        
+    plt.tight_layout(rect=[0.03, 0, 1, 1])
+    plt.show()
+
+def plot_raw_rates(
+        dataframe: pd.DataFrame,
+        start_time: float | None = None,
+        end_time: float | None = None,
+        plot_labels: dict | None = None
+        ):
+    
+    if dataframe is None:
+        raise ValueError("No DataFrame provided.")
+    if dataframe.empty:
+        raise ValueError("DataFrame is empty.")
+    if 'timestamp' not in dataframe.columns:
+        raise ValueError("DataFrame has no 'timestamp' column.")
+    if plot_labels is None:
+        plot_labels = {}
+        
+    fig, axs = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
+    base_title = "Flight Performance - Figure 2"
+    subtitle = plot_labels.get("subtitle", "Last Flight Test")
+    full_title = f"{base_title}\n{subtitle}" if subtitle else base_title
+    fig.suptitle(full_title, fontsize=14, weight='bold')
+        
+    if start_time is not None:
+        dataframe = dataframe[dataframe["timestamp"] >= start_time]
+    if end_time is not None:
+        dataframe = dataframe[dataframe["timestamp"] <= end_time]
+
+    time = dataframe["timestamp"]
+    rol_rate = dataframe["gx_raw"]
+    pit_rate = dataframe["gy_raw"]
+    yaw_rate = dataframe["gz_raw"]
+    
+    axs[0].scatter(time, rol_rate, label='Roll', color="blue")
+    axs[1].scatter(time, pit_rate, label='Pitch', color="red")
+    axs[2].scatter(time, yaw_rate, label='Yaw', color="green")
     
     # --- Final Formatting ---
     axs[0].set_title("Roll Rate Over Time")
@@ -240,9 +292,9 @@ def plot_angles(
     pit_deg = dataframe["pitch_deg"]
     yaw_deg = _recenter_angles(dataframe["yaw_deg"])
     
-    axs[0].plot(time, rol_deg, label='Roll', color="blue")
-    axs[1].plot(time, pit_deg, label='Pitch', color="red")
-    axs[2].plot(time, yaw_deg, label='Yaw', color="green")
+    axs[0].scatter(time, rol_deg, label='Roll', color="blue")
+    axs[1].scatter(time, pit_deg, label='Pitch', color="red")
+    axs[2].scatter(time, yaw_deg, label='Yaw', color="green")
     
     # --- Final Formatting ---
     axs[0].set_title("Roll Angle Over Time")
@@ -298,7 +350,7 @@ def plot_energy(
     axs[0].set_title("Airspeed Over Time")
     axs[0].set_ylabel("Airspeed\n[deg]")
     axs[1].set_title("Altitude Over Time")
-    axs[1].set_ylabel("Altitude\n[]")
+    axs[1].set_ylabel("Altitude\n[m]")
     
     for ax in axs:
         ax.grid(True)
@@ -337,12 +389,83 @@ def plot_trajectory(
 
     time = dataframe["timestamp"]
     ail_def = dataframe["roll_cmd"]
-    elv_def = dataframe["pitch_cmd"]
-    rud_def = dataframe["yaw_cmd"]
+    elv_def = dataframe["yaw_cmd"]
+    rud_def = dataframe["pitch_cmd"]
     
-    axs[0].plot(time, ail_def, label='Aileron', color="blue", linestyle="-")
-    axs[1].plot(time, elv_def, label='Elevator', color="red", linestyle="-")
-    axs[2].plot(time, rud_def, label='Rudder', color="green", linestyle="-")
+    axs[0].scatter(time, ail_def, label='Aileron', color="blue", linestyle="-")
+    axs[1].scatter(time, elv_def, label='Elevator', color="red", linestyle="-")
+    axs[2].scatter(time, rud_def, label='Rudder', color="green", linestyle="-")
+    
+    # --- Final Formatting ---
+    axs[0].set_title("Aileron Trajectory Over Time")
+    axs[0].set_ylabel("Deflection\n[deg]")
+    axs[1].set_title("Elevator Trajectory Over Time")
+    axs[1].set_ylabel("Deflection\n[deg]")
+    axs[2].set_title("Rudder Trajectory Over Time")
+    axs[2].set_ylabel("Deflection\n[deg]")
+    
+    for ax in axs:
+        ax.grid(True)
+        ax.legend(loc='upper right', fontsize='medium')
+    axs[-1].set_xlabel("Time [s]")
+        
+    plt.tight_layout(rect=[0.03, 0, 1, 1])
+    plt.show()
+    
+def plot_input_performance(
+        trajectory_dataframe: pd.DataFrame,
+        odometry_dataframe: pd.DataFrame,
+        start_time: float | None = None,
+        end_time: float | None = None,
+        plot_labels: dict | None = None
+        ):
+    
+    if trajectory_dataframe is None:
+        raise ValueError("No DataFrame provided.")
+    if trajectory_dataframe.empty:
+        raise ValueError("DataFrame is empty.")
+    if 'timestamp' not in trajectory_dataframe.columns:
+        raise ValueError("DataFrame has no 'timestamp' column.")
+    if odometry_dataframe is None:
+        raise ValueError("No DataFrame provided.")
+    if odometry_dataframe.empty:
+        raise ValueError("DataFrame is empty.")
+    if 'timestamp' not in odometry_dataframe.columns:
+        raise ValueError("DataFrame has no 'timestamp' column.")
+    if plot_labels is None:
+        plot_labels = {}
+        
+    fig, axs = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
+    base_title = "Flight Performance - Figure 6"
+    subtitle = plot_labels.get("subtitle", "Last Flight Test")
+    full_title = f"{base_title}\n{subtitle}" if subtitle else base_title
+    fig.suptitle(full_title, fontsize=14, weight='bold')
+        
+    if start_time is not None:
+        trajectory_dataframe = trajectory_dataframe[trajectory_dataframe["timestamp"] >= start_time]
+    if end_time is not None:
+        trajectory_dataframe = trajectory_dataframe[trajectory_dataframe["timestamp"] <= end_time]
+    if start_time is not None:
+        odometry_dataframe = odometry_dataframe[odometry_dataframe["timestamp"] >= start_time]
+    if end_time is not None:
+        odometry_dataframe = odometry_dataframe[odometry_dataframe["timestamp"] <= end_time]
+
+    trajectory_time = trajectory_dataframe["timestamp"]
+    ail_def = trajectory_dataframe["roll_cmd"]
+    elv_def = trajectory_dataframe["yaw_cmd"]
+    rud_def = trajectory_dataframe["pitch_cmd"]
+    
+    odometry_time = odometry_dataframe["timestamp"]
+    rol_deg = odometry_dataframe["roll_deg"]
+    pit_deg = odometry_dataframe["pitch_deg"]
+    yaw_deg = _recenter_angles(odometry_dataframe["yaw_deg"])
+    
+    axs[0].plot(trajectory_time, ail_def, label='Desired', color="black", linestyle="--")
+    axs[1].plot(trajectory_time, elv_def, label='Desired', color="black", linestyle="--")
+    axs[2].plot(trajectory_time, rud_def, label='Desired', color="black", linestyle="--")
+    axs[0].plot(odometry_time, rol_deg, label='Actual', color="blue", linestyle="-")
+    axs[1].plot(odometry_time, pit_deg, label='Actual', color="red", linestyle="-")
+    axs[2].plot(odometry_time, yaw_deg, label='Actual', color="green", linestyle="-")
     
     # --- Final Formatting ---
     axs[0].set_title("Aileron Trajectory Over Time")
@@ -365,39 +488,20 @@ def plot_trajectory(
 if __name__ == "__main__":
     get_ipython().run_line_magic('matplotlib', 'qt')
     # get_ipython().run_line_magic('matplotlib', 'inline')
-    # csv_path = "M:/cUAS Unclassified/125 - Mosley/OLS Model Results/synced_all_data.csv"
-    # csv_path = "M:/cUAS Unclassified/125 - Mosley/OLS Model Results/ols_rol_data.csv"
-    
-    # models = ['ols_rol_']
-    
-    start_time = 26
-    end_time = 38
-    # plot_labels = {
-    # "subtitle": "Roll Models",
-    # "time": "Time [s]",
-    # "measured_output": "Measured Roll\nAcceleration [deg/s²]",
-    # "output_amp": "Roll Acceleration\n[deg/s²]",
-    # "output_percent_confidence": "Confidence [%]",
-    # "cod_amp": "R²",
-    # "residuals": "Roll Acceleration\nResiduals [deg/s²]",
-    # "mse": "Roll Acceleration\nSquared Error\n[(deg/s²)²]",
-    # "param_1_amp": "Roll Velocity\nParameter [1/s]",
-    # "param_2_amp": "Aileron Parameter\n[1/s²]",
-    # "param_3_amp": "Yaw Velocity\nParameter [1/s]",
-    # "param_4_amp": "Rudder Parameter\n[1/s²]",
-    # "param_1_cod_amp": "Roll Velocity\nParameter's r²\n[%]",
-    # "param_2_cod_amp": "Aileron\nParameter's r²\n[%]",
-    # "param_3_cod_amp": "Yaw Velocity\nParameter's r²\n[%]",
-    # "param_4_cod_amp": "Rudder\nParameter's r²\n[%]",
-    # "param_1_cond_amp": "Roll Velocity\nParameter's\nConditioning",
-    # "param_2_cond_amp": "Aileron\nParameter's\nConditioning",
-    # "param_3_cond_amp": "Yaw Velocity\nParameter's\nConditioning",
-    # "param_4_cond_amp": "Rudder\nParameter's\nConditioning",
-    # }
 
-    plot_overall(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/synced_all_data.csv"))
-    plot_signals(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/rcout_data.csv"))
-    plot_rates(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/imu_data.csv"))
-    plot_angles(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/odometry_data.csv"))
-    plot_energy(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/synced_all_data.csv"))
-    plot_trajectory(pd.read_csv("M:/cUAS Unclassified/125 - Mosley/OLS Model Results/trajectory_data.csv"))
+    # Day1_1714: 135.5-165.5, 345.75-375.75
+    # Day1_1722: 16-46, 126-156, 222-252
+    # Day2_1459: 18-48, 118-148, 218-248
+    start_time = 0
+    end_time = 9999
+    
+    folder_path = "/home/cuav/Desktop/coding_projects/ANTX25/Topic CSVs/Day5_1416/"
+    
+    # plot_overall(pd.read_csv(f"{folder_path}synced_all_data.csv"), start_time, end_time)
+    # plot_signals(pd.read_csv(f"{folder_path}rcout_data.csv"), start_time, end_time)
+    plot_rates(pd.read_csv(f"{folder_path}imu_data.csv"), start_time, end_time)
+    # plot_raw_rates(pd.read_csv(f"{folder_path}imu_raw_data.csv"), start_time, end_time)
+    # plot_angles(pd.read_csv(f"{folder_path}odometry_data.csv"), start_time, end_time)
+    plot_energy(pd.read_csv(f"{folder_path}synced_all_data.csv"), start_time, end_time)
+    # plot_trajectory(pd.read_csv(f"{folder_path}trajectory_data.csv"), start_time, end_time)
+    plot_input_performance(pd.read_csv(f"{folder_path}trajectory_data.csv"), pd.read_csv(f"{folder_path}odometry_data.csv"), start_time, end_time)
